205496ee3189309f30df10ba9b2ea0e271cfaf78
gurprit-singh <75479921+gurprit-singh@users.noreply.github.com>
2025-10-21 11:52:03 +0530

    auto genrate pages wip

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,26 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import Kannad from "./pages/Kannad";
+import Punjabi from "./pages/Punjabi";
 import Korean from "./pages/korean";
+import Hindi from "./pages/Hindi";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/yellow" element={<Index />} />
-          <Route path="/korean" element={<Korean />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/korean" element={<Korean />} />
+        <Route path="/hindi" element={<Hindi />} />
+        <Route path="/punjabi" element={<Punjabi />} />
+        <Route path="/kannad" element={<Kannad />} />
+        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </QueryClientProvider>
 );
 
