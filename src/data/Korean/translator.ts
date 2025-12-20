99eeeefd5ef23f288ae047b802bc1f4e52fe281f
gurprit-singh <75479921+gurprit-singh@users.noreply.github.com>
2025-12-21 00:20:05 +0530

    wip

--- src/data/Korean/translator.ts
+++ src/data/Korean/translator.ts
@@ -1,6 +1,6 @@
 const data = {
     heading: "Certified Korean Translator Service in India",
-    subHeading: "Triasia Global Translation Services Govt approved Translation Agency in India. We provide Certified Korean Translator Services in India for Legal Documents, Marriage Certificates, Educational Certificates, Driving Licenses, Visa & Immigration Documents, etc. Our Govt Authorized Korean Translators assure 100% certified & legal Korean Translation service with high accuracy at reasonable prices."
+    subHeading: "Triasia global translation services is a govt approved Translation Agency in India. We provide Certified Korean Translator Services in India for Legal Documents, Marriage Certificates, Educational Certificates, Driving Licenses, Visa & Immigration Documents, etc. Our Govt Authorized Korean Translators assure 100% certified & legal Korean Translation service with high accuracy at reasonable prices."
 }
 
 export default data;
